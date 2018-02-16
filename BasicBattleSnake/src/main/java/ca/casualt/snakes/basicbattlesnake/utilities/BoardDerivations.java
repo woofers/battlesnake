ec68f91ece8dcae46fcc360b52ccd47dc53e52d6
Tony <tflinz@yahoo.ca>
2018-02-15 18:29:15 -0800

    Cleanup.

--- BasicBattleSnake/src/main/java/ca/casualt/snakes/basicbattlesnake/utilities/BoardDerivations.java
+++ BasicBattleSnake/src/main/java/ca/casualt/snakes/basicbattlesnake/utilities/BoardDerivations.java
@@ -2,12 +2,9 @@ package ca.casualt.snakes.basicbattlesnake.utilities;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import ca.casualt.snakes.basicbattlesnake.types.BoardSpec;
-import ca.casualt.snakes.basicbattlesnake.types.MoveRequest;
 import ca.casualt.snakes.basicbattlesnake.types.Point;
-import ca.casualt.snakes.basicbattlesnake.types.Snake;
 
 /**
  * For deriving pieces of handy information about the board from the base data
@@ -56,58 +53,6 @@ public class BoardDerivations {
 			// System.out.println("(" + x + "," + y + ")");
 		}
 		return toReturn;
-	}
-
-	/**
-	 * This will generate the list of points that make up the board.
-	 *
-	 * @param boardSpec
-	 * @return
-	 */
-	public static List<Point> generateBoardPoints(final BoardSpec boardSpec) {
-		List<Point> toReturn = new ArrayList<>();
-		for (int x = 0; x < boardSpec.getWidth(); x++) {
-			for (int y = 0; y < boardSpec.getHeight(); y++) {
-				toReturn.add(new Point(x, y));
-			}
-		}
-		return toReturn;
-	}
-
-	/**
-	 * This will generate the list of points that are occupied by snakes.
-	 *
-	 * @param moveRequest
-	 * @return
-	 */
-	public static List<Point> generateOccupiedPoints(final MoveRequest moveRequest) {
-		return moveRequest.getSnakes().stream().map(Snake::getBody).flatMap(List::stream).collect(Collectors.toList());
-	}
-
-	/**
-	 *
-	 * @param moveRequest
-	 * @return
-	 */
-	public static List<Point> generateUnoccupiedPoints(final MoveRequest moveRequest) {
-		List<Point> totalPoints = generateBoardPoints(moveRequest);
-		List<Point> occupiedPoints = generateOccupiedPoints(moveRequest);
-		totalPoints.removeAll(occupiedPoints);
-		return totalPoints;
-	}
-
-	/**
-	 * Calculate the average/open-center. Relative to current board state.
-	 *
-	 * @param moveRequest
-	 * @return
-	 */
-	public static Point calculateOpenCenter(final MoveRequest moveRequest) {
-		List<Point> unoccupiedPoints = generateUnoccupiedPoints(moveRequest);
-		final double averageX = unoccupiedPoints.stream().map(Point::getX).collect(Collectors.averagingInt(x -> x));
-		final double averageY = unoccupiedPoints.stream().map(Point::getY).collect(Collectors.averagingInt(y -> y));
-		System.out.println("(" + averageX + "," + averageY + ")");
-		return new Point((int) averageX, (int) averageY);
 	}
 
 }
