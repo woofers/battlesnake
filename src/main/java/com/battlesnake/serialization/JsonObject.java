719d966420f0b4e7e86f40c1cc7a91ddb7c50967
Jaxson Van Doorn <woofers1tt@gmail.com>
2019-02-24 16:33:55 -0800

    Use lower camel case for snakes responses
    Uses a TypeAdapterFactor to ensure enums are the correct case.

--- src/main/java/com/battlesnake/serialization/JsonObject.java
+++ src/main/java/com/battlesnake/serialization/JsonObject.java
@@ -3,14 +3,13 @@ package com.battlesnake.serialization;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.interceptors.InterceptorFactory;
-import com.google.gson.FieldNamingPolicy;
 
 public abstract class JsonObject {
 
     protected Gson gson() {
         return new GsonBuilder()
-            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
             .registerTypeAdapterFactory(new InterceptorFactory())
+            .registerTypeAdapterFactory(new LowercaseEnumTypeAdapterFactory())
             .create();
     }
 
