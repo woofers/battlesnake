d66f15617c231dd46a7040c2ceefeb1e2ac87c7a
Jaxson Van Doorn <woofers1tt@gmail.com>
2019-03-01 11:43:55 -0800

    Remove redundant case for determining tail location  (#57)
    Since the only time the snake has one body part is at
    the start of the game (when `body.size() > 1`), then the
    check to ensure it is not turn 0 (`turn != 0`) is redundant.

--- src/main/java/com/battlesnake/game/Board.java
+++ src/main/java/com/battlesnake/game/Board.java
@@ -259,7 +259,7 @@ public class Board {
             for (int i = 0; i < body.size(); i++) {
                 if ((i == body.size() - 1)
                     && body.size() > 1
-                    && !(snake.justAte() && turn != 0)) {
+                    && !snake.justAte()) {
                     board[body.get(i).getX()][body.get(i).getY()] = Tile.TAIL;
                 }
                 else {
