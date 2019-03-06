1a4dfb792eb315b423a3f68ab20ccc6818ef4b71
Jaxson Van Doorn <woofers1tt@gmail.com>
2019-03-05 22:27:54 -0800

    Use constant for JSON response type

--- src/main/java/com/battlesnake/endpoints/Endpoint.java
+++ src/main/java/com/battlesnake/endpoints/Endpoint.java
@@ -6,12 +6,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletResponse;
 
+import javax.ws.rs.core.MediaType;
+
 @SuppressWarnings("serial")
 public class Endpoint extends HttpServlet {
 
     protected static void respond(String body, HttpServletResponse response)
         throws ServletException, IOException {
-        response.setContentType("application/json");
+        response.setContentType(MediaType.APPLICATION_JSON);
         response.getWriter().println(body);
     }
 }
