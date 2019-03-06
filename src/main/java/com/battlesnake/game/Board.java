f21e05373e6ca6ae73abe1f88c77d04e83f364a7
Jaxson Van Doorn <woofers1tt@gmail.com>
2019-03-05 21:36:09 -0800

    Remove use of deprecated integer constructor
    `new Integer(int)` is deprecated in Java 9

--- src/main/java/com/battlesnake/game/Board.java
+++ src/main/java/com/battlesnake/game/Board.java
@@ -32,7 +32,7 @@ public class Board {
         TAIL;
 
         public String toString() {
-            return new Integer(this.ordinal()).toString();
+            return Integer.valueOf(this.ordinal()).toString();
         }
     }
 
