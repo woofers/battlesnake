ba698f60482c9729be93cb62343751388c1d5559
Ben Austin <austinlhom@gmail.com>
2020-04-16 17:16:43 -0700

    Fix syntax error causing failed build

--- src/main/java/com/battlesnake/game/snake/SnakeConfig.java
+++ src/main/java/com/battlesnake/game/snake/SnakeConfig.java
@@ -10,6 +10,6 @@ public final class SnakeConfig extends JsonObject {
 
     @Getter private final String name = "SLIME LANGUAGE";
     @Getter private final String color = "#8FD628";
-    @Getter private final Head headType = Head.SAND_WORM
+    @Getter private final Head headType = Head.SAND_WORM;
     @Getter private final Tail tailType = Tail.SHARP;
 }
