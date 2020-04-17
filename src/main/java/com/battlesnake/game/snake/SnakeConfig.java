c44c643a2910557df32d756df51a80edf95eea2a
Ben Austin <austinlhom@gmail.com>
2020-04-16 18:30:47 -0700

    Update head, tail, and color for Walter (#115)

--- src/main/java/com/battlesnake/game/snake/SnakeConfig.java
+++ src/main/java/com/battlesnake/game/snake/SnakeConfig.java
@@ -8,8 +8,8 @@ import com.battlesnake.serialization.JsonObject;
 @Accessors(fluent = true)
 public final class SnakeConfig extends JsonObject {
 
-    @Getter private final String name = "🐍 ‏‏‎ 𝙎𝙐𝙋𝙀𝙍 𝙎𝙇𝙄𝙈𝙀𝙔 ‏‏‎ 🐍";
-    @Getter private final String color = "#8FD628";
-    @Getter private final Head headType = Head.EVIL;
-    @Getter private final Tail tailType = Tail.BOLT;
+    @Getter private final String name = "SLIME LANGUAGE";
+    @Getter private final String color = "#FFFFFF";
+    @Getter private final Head headType = Head.SAFE;
+    @Getter private final Tail tailType = Tail.SMALL_RATTLE;
 }
